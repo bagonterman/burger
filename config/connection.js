2432b190f7d6e697e1cd6d1ea548a96e8ddf8208
Brett Gonterman <brettgonterman@sbcglobal.net>
2018-08-22 18:29:59 -0500

    burger

--- config/connection.js
+++ config/connection.js
@@ -4,6 +4,7 @@ let connection;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
+
 } else {
     connection = mysql.createConnection({
         host: "localhost",
